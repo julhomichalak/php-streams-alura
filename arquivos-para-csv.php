069fa72d70ceb9e5fd9167923cfbf09cdb1007db
Julho Michalak <julhomichalak@gmail.com>
2023-07-16 20:08:43 -0300

    finalizando

---
+++ arquivos-para-csv.php
@@ -0,0 +1,20 @@
+<?php
+
+$meusCursos = file('lista-cursos.txt');
+$outrosCursos = file('cursos-php.txt');
+
+$arquivoCsv = fopen('cursos.csv', 'w');
+
+foreach ($meusCursos as $curso) {
+    $linha = [trim(mb_convert_encoding($curso,"Windows-1252","UTF-8")), 'Sim'];
+
+    fputcsv($arquivoCsv, $linha, ';');
+}
+
+foreach ($outrosCursos as $curso) {
+    $linha = [trim(mb_convert_encoding($curso,"Windows-1252","UTF-8")), 'Não'];
+
+    fputcsv($arquivoCsv, $linha, ';');
+}
+
+fclose($arquivoCsv);
