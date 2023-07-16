54e523b69789a6860e1b1203ca485cec1ec5a829
Julho Michalak <julhomichalak@gmail.com>
2023-07-15 21:25:32 -0300

    trabalhando com .zip com senha

--- leitor-zip.php
+++ leitor-zip.php
@@ -1,9 +1,16 @@
 <?php
 
-echo file_get_contents('zip://arquivo.zip#lista-cursos.txt');
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => '123456'
+    ]
+]);
+echo file_get_contents('zip://arquivo.zip#lista-cursos.txt',
+ false, 
+ $contexto);
 
 
-
+//fopen(filename: 'arquivo', mode: 'modo', use_include_path: false, $contexto)
 
 
 
